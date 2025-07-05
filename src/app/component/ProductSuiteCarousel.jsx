92679c6b0a092bf6eced2ea5993450f11b242787
Ajaysharma <akpancholi8@gmail.com>
2025-07-05 21:18:01 +0530

    commit

--- src/app/component/ProductSuiteCarousel.jsx
+++ src/app/component/ProductSuiteCarousel.jsx
@@ -59,7 +59,7 @@ export default function ProductSuiteCarousel() {
                 : ""
                 }`}
             >
-              Aasvaa Aera
+              AasvaaEra 
             </h5>
           </div>
           <div>
